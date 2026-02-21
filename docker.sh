c1945734c2e4fa8220222c5e536e1451b992d247
Kelyan <advertise@kelyanrodiergilson.fr>
2026-02-21 14:41:08 +0100

    New install docker

---
+++ docker.sh
@@ -0,0 +1,54 @@
+#!/bin/bash
+if command -v docker &> /dev/null
+then
+  echo "Docker est déjà installé"
+  exit 1
+fi
+
+if [ -f /etc/os-release ]; then
+    . /etc/os-release
+    OS=$ID
+else
+    echo "Impossible de détecter la distribution Linux..."
+    exit 1
+fi
+
+echo "Installation de Docker pour $OS..."
+
+case "$OS" in
+    ubuntu|debian|raspbian|pop)
+        apt-get update
+        apt-get install -y ca-certificates curl gnupg
+        install -m 0755 -d /etc/apt/keyrings
+        curl -fsSL https://download.docker.com/linux/$OS/gpg | gpg --dearmor -o /etc/apt/keyrings/docker.gpg --yes
+        chmod a+r /etc/apt/keyrings/docker.gpg
+
+        echo "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/$OS $VERSION_CODENAME stable" | \
+        tee /etc/apt/sources.list.d/docker.list > /dev/null
+        
+        apt-get update
+        apt-get install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+        ;;
+
+    fedora)
+        dnf -y install dnf-plugins-core
+        dnf config-manager --add-repo https://download.docker.com/linux/fedora/docker-ce.repo
+        dnf install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+        ;;
+
+    centos|rhel|almalinux|rocky)
+        yum install -y yum-utils
+        yum-config-manager --add-repo https://download.docker.com/linux/centos/docker-ce.repo
+        yum install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+        ;;
+
+    *)
+        echo "Distribution $OS non supportée par ce script."
+        exit 1
+        ;;
+esac
+
+echo "$Démarrage et activation du service Docker..."
+systemctl enable --now docker
+
+echo "Docker a été installé avec succès sous la version $(docker --version)"
